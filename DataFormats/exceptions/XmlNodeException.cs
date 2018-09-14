9bfcf7d9f81a32476f3949c2c23cb3c24ce652bc
J M <wohaste@gmail.com>
2018-09-14 16:31:07 -0600

    moving XmlDocument code from Tamarin to here

---
+++ DataFormats/exceptions/XmlNodeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutHaste.DataFormats
+{
+	/// <summary>
+	/// Exceptions related to XmlNode objects.
+	/// </summary>
+	public class XmlNodeException : Exception
+	{
+		public XmlNodeException(string message) : base(message)
+		{
+		}
+	}
+}
