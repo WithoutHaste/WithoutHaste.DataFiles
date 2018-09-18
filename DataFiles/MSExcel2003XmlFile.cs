098ff64a984b0ee353893536c8b70600e7abdd2a
J M <wohaste@gmail.com>
2018-09-18 16:20:29 -0600

    documentation

--- DataFiles/MSExcel2003XmlFile.cs
+++ DataFiles/MSExcel2003XmlFile.cs
@@ -15,7 +15,14 @@ namespace WithoutHaste.DataFiles
 	/// <code><![CDATA[
 	/// <? xml version="1.0" encoding="UTF-8"?>
 	/// <? mso-application progid="Excel.Sheet" ?>
-	/// <Workbook xmlns:c="urn:schemas-microsoft-com:office:component:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:x2="http://schemas.microsoft.com/office/excel/2003/xml" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:x="urn:schemas-microsoft-com:office:excel">
+	/// <Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" 
+	///   xmlns:c="urn:schemas-microsoft-com:office:component:spreadsheet" 
+	///   xmlns:html="http://www.w3.org/TR/REC-html40" 
+	///   xmlns:o="urn:schemas-microsoft-com:office:office" 
+	///   xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" 
+	///   xmlns:x="urn:schemas-microsoft-com:office:excel" 
+	///   xmlns:x2="http://schemas.microsoft.com/office/excel/2003/xml" 
+	///   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"> 
 	///   <ss:Worksheet ss:Name="Sheet1">
 	///     <Table>
 	///       <Column />
