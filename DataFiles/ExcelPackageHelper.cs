330150b2b915cbacf87174c68d9ebd53550a5514
J M <wohaste@gmail.com>
2018-09-18 15:31:38 -0600

    test cases

--- DataFiles/ExcelPackageHelper.cs
+++ DataFiles/ExcelPackageHelper.cs
@@ -7,110 +7,304 @@ using OfficeOpenXml;
 
 namespace WithoutHaste.DataFiles
 {
+	/// <summary>Extensions for ExcelPackage.</summary>
 	public static class ExcelPackageHelper
 	{
+		public const int MIN_ROW_INDEX = 1;
+		public const int MIN_COLUMN_INDEX = 1;
+		public const string MIN_COLUMN_CHAR = "A";
+
+		/// <summary>
+		/// Add a new Worksheet to an ExcelPackage.
+		/// </summary>
+		/// <param name="excelPackage"></param>
+		/// <param name="name">The name of the Worksheet.</param>
+		/// <returns>The new Worksheet.</returns>
+		/// <exception cref="ArgumentException">ExcelPackage cannot be null.</exception>
 		public static ExcelWorksheet AddWorksheet(ExcelPackage excelPackage, string name)
 		{
+			ExcelPackageCannotBeNull(excelPackage);
+
 			return excelPackage.Workbook.Worksheets.Add(name);
 		}
 
+		/// <summary>
+		/// Searches the ExcelPackage for a Worksheet by name.
+		/// </summary>
+		/// <param name="excelPackage"></param>
+		/// <param name="name">The name of the Worksheet.</param>
+		/// <returns>The Worksheet, or null.</returns>
+		/// <exception cref="ArgumentException">ExcelPackage cannot be null.</exception>
 		public static ExcelWorksheet GetWorksheet(ExcelPackage excelPackage, string name)
 		{
+			ExcelPackageCannotBeNull(excelPackage);
+
 			return excelPackage.Workbook.Worksheets[name];
 		}
 
+		/// <duplicate cref="ExcelPackageHelper.AppendRow" />
+		public static void AppendRow(ExcelWorksheet worksheet, List<string> values)
+		{
+			AppendRow(worksheet, values.Cast<object>().ToList());
+		}
+
+		/// <duplicate cref="ExcelPackageHelper.AppendRow" />
+		public static void AppendRow(ExcelWorksheet worksheet, List<int> values)
+		{
+			AppendRow(worksheet, values.Cast<object>().ToList());
+		}
+
+		/// <duplicate cref="ExcelPackageHelper.AppendRow" />
+		public static void AppendRow(ExcelWorksheet worksheet, List<decimal> values)
+		{
+			AppendRow(worksheet, values.Cast<object>().ToList());
+		}
+
+		/// <summary>
+		/// Add a new row of data to the end of the Worksheet.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="values">The data values for the row.</param>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="ArgumentException">Values list cannot be null.</exception>
 		public static void AppendRow(ExcelWorksheet worksheet, List<object> values)
 		{
+			WorksheetCannotBeNull(worksheet);
+			if(values == null)
+				throw new ArgumentException("Values list cannot be null.");
+
 			int row = CountRows(worksheet) + 1;
 			worksheet.InsertRow(row, 1);
-			char column = 'A';
+			int column = MIN_COLUMN_INDEX;
 			foreach(object value in values)
 			{
-				worksheet.Cells[String.Format("{0}{1}", column, row)].Value = value;
+				worksheet.Cells[row, column].Value = value;
+				column++;
 			}
 		}
 
-		public static void SetColumn(ExcelWorksheet worksheet, string column, List<object> values, bool skipFirstRow = true)
+		/// <summary>
+		/// Returns all the values from one row, specified by the 0-indexed row number.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="row">Row number, starting at 1.</param>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="IndexOutOfRangeException">Row index out of range.</exception>
+		public static List<object> GetRow(ExcelWorksheet worksheet, int row)
 		{
-			int row = (skipFirstRow) ? 2 : 1;
+			WorksheetCannotBeNull(worksheet);
+			if(row > CountRows(worksheet))
+				throw new IndexOutOfRangeException(String.Format("Row index out of range [1,{0}]: {1}", CountRows(worksheet), row));
+
+			List<object> values = new List<object>();
+			for(int i = MIN_COLUMN_INDEX; i <= CountColumns(worksheet); i++)
+			{
+				values.Add(worksheet.Cells[ColumnChar(i) + row].Value);
+			}
+			return values;
+		}
+
+		/// <summary>
+		/// Set an entire column of values at once.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="column">Integer index of column, starting at 1.</param>
+		/// <param name="values">Data values for the column.</param>
+		/// <param name="skipFirstRow">If true, values[0] is applied to row 2 instead of 1. Intended for indicating the first row is for headers.</param>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="ArgumentException">Values list cannot be null.</exception>
+		public static void SetColumnByIndex(ExcelWorksheet worksheet, int column, List<object> values, bool skipFirstRow = true)
+		{
+			SetColumnByChar(worksheet, ColumnChar(column), values, skipFirstRow);
+		}
+
+		/// <summary>
+		/// Set an entire column of values at once.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="header">Header value of column.</param>
+		/// <param name="values">Data values for the column. Should not include the header.</param>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="ArgumentException">Values list cannot be null.</exception>
+		public static void SetColumnByHeader(ExcelWorksheet worksheet, string header, List<object> values)
+		{
+			SetColumnByChar(worksheet, GetColumnCharForHeader(worksheet, header), values, skipFirstRow:true);
+		}
+
+		/// <summary>
+		/// Set an entire column of values at once.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="column">Character name of column. Example: "A", "Z", "AA".</param>
+		/// <param name="values">Data values for the column.</param>
+		/// <param name="skipFirstRow">If true, values[0] is applied to row 2 instead of 1. Intended for indicating the first row is for headers.</param>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="ArgumentException">Values list cannot be null.</exception>
+		public static void SetColumnByChar(ExcelWorksheet worksheet, string column, List<object> values, bool skipFirstRow = true)
+		{
+			WorksheetCannotBeNull(worksheet);
+			if(values == null)
+				throw new ArgumentException("Values list cannot be null.");
+
+			int row = MIN_ROW_INDEX;
+			if(skipFirstRow) row++;
 			foreach(object value in values)
 			{
 				if(CountRows(worksheet) < row)
 					worksheet.InsertRow(row, 1);
 				worksheet.Cells[String.Format("{0}{1}", column, row)].Value = value;
+				row++;
 			}
 		}
 
+		/// <summary>
+		/// Searches first row for a particular value.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="header">Header value.</param>
+		/// <returns>Character name of the header's column, or null. Example: "A", "Z", "AA".</returns>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		/// <exception cref="IndexOutOfRangeException">No header row found.</exception>
 		public static string GetColumnCharForHeader(ExcelWorksheet worksheet, string header)
 		{
-			for(int column = 1; column <= CountColumns(worksheet); column++)
+			List<object> headers = GetRow(worksheet, MIN_ROW_INDEX);
+			for(int i = 0; i < headers.Count; i++)
 			{
-				if(worksheet.Cells[String.Format("{0}0", ColumnChar(column))].Value.ToString() == header)
-				{
-					return ColumnChar(column);
-				}
+				if(headers[i] != null && headers[i].ToString() == header)
+					return ColumnChar(i + 1);
 			}
 			return null;
 		}
 
+		/// <summary>
+		/// Returns all the values from one column, specified by the header value.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="header"></param>
+		/// <returns>List of data values, not including the header. Includes all cells to the bottom of the Worksheet.</returns>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
 		public static List<object> GetColumnByHeader(ExcelWorksheet worksheet, string header)
 		{
-			for(int column = 1; column <= CountColumns(worksheet); column++)
-			{
-				if(worksheet.Cells[String.Format("{0}0", ColumnChar(column))].Value.ToString() == header)
-				{
-					return GetColumnByChar(worksheet, ColumnChar(column), skipFirstRow:true);
-				}
-			}
-			return new List<object>();
+			return GetColumnByChar(worksheet, GetColumnCharForHeader(worksheet, header), skipFirstRow: true);
 		}
 
+		/// <summary>
+		/// Returns all values from one column, specified by the integer index of the column.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="column">Integer index of column, starting at 1.</param>
+		/// <param name="skipFirstRow">If true, the first value in the column is not included. Intended for skipping the header value.</param>
+		/// <returns>List of data values. Includes all cells to the bottom of the Worksheet.</returns>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		public static List<object> GetColumnByIndex(ExcelWorksheet worksheet, int column, bool skipFirstRow = true)
+		{
+			return GetColumnByChar(worksheet, ColumnChar(column), skipFirstRow);
+		}
+
+		/// <summary>
+		/// Returns all values from one column, specified by the character name of the column.
+		/// </summary>
+		/// <param name="worksheet"></param>
+		/// <param name="column">Character name of column. Example: "A", "Z", "AA".</param>
+		/// <param name="skipFirstRow">If true, the first value in the column is not included. Intended for skipping the header value.</param>
+		/// <returns>List of data values. Includes all cells to the bottom of the Worksheet.</returns>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
 		public static List<object> GetColumnByChar(ExcelWorksheet worksheet, string column, bool skipFirstRow=true)
 		{
+			WorksheetCannotBeNull(worksheet);
+
 			List<object> values = new List<object>();
-			for(int row = (skipFirstRow ? 2 : 1); row <= CountRows(worksheet); row++)
+			for(int row = (skipFirstRow ? (MIN_ROW_INDEX+1) : MIN_ROW_INDEX); row <= CountRows(worksheet); row++)
 			{
 				values.Add(worksheet.Cells[column + row].Value);
 			}
 			return values;
 		}
 
-		public static int CountRows(ExcelWorksheet excelWorksheet)
+		/// <summary>
+		/// Returns the number of rows in the Worksheet.
+		/// </summary>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		public static int CountRows(ExcelWorksheet worksheet)
 		{
-			return excelWorksheet.Dimension.End.Row;
+			WorksheetCannotBeNull(worksheet);
+
+			if(worksheet.Dimension == null)
+				return 0;
+			return worksheet.Dimension.End.Row;
 		}
 
-		public static int CountColumns(ExcelWorksheet excelWorksheet)
+		/// <summary>
+		/// Returns the number of columns in the Worksheet.
+		/// </summary>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
+		public static int CountColumns(ExcelWorksheet worksheet)
 		{
-			return excelWorksheet.Dimension.End.Column;
+			WorksheetCannotBeNull(worksheet);
+
+			if(worksheet.Dimension == null)
+				return 0;
+			return worksheet.Dimension.End.Column;
+		}
+
+		/// <summary>
+		/// Returns the number of Worksheets in the ExcelPacakge.
+		/// </summary>
+		/// <exception cref="ArgumentException">ExcelPackage cannot be null.</exception>
+		public static int CountWorksheets(ExcelPackage excelPackage)
+		{
+			ExcelPackageCannotBeNull(excelPackage);
+
+			return excelPackage.Workbook.Worksheets.Count;
 		}
 
 		/// <summary>
 		/// Convert column integer index to character index.
 		/// </summary>
 		/// <example>1 becomes A</example>
-		/// <example>26 becomes B</example>
+		/// <example>26 becomes Z</example>
 		/// <example>27 becomes AA</example>
-		/// <param name="columnNumber">1-based index</param>
+		/// <param name="columnNumber">1-based index of column</param>
 		public static string ColumnChar(int columnNumber)
 		{
 			int dividend = columnNumber;
 			string columnName = String.Empty;
 			int modulo;
 
+			int ALPHABET_LENGTH = 26;
+			int A_CHAR_VALUE = 65;
+
 			while(dividend > 0)
 			{
-			modulo = (dividend - 1) % 26;
-			columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
-			dividend = (int)((dividend - modulo) / 26);
+				modulo = (dividend - 1) % ALPHABET_LENGTH;
+				columnName = Convert.ToChar(A_CHAR_VALUE + modulo).ToString() + columnName;
+				dividend = (int)((dividend - modulo) / ALPHABET_LENGTH);
 			}
 
 			return columnName;
 		}
 
+		/// <summary>
+		/// Remove all rows and columns from Worksheet.
+		/// </summary>
+		/// <exception cref="ArgumentException">Worksheet cannot be null.</exception>
 		public static void Clear(ExcelWorksheet worksheet)
 		{
-			worksheet.DeleteRow(rowFrom: 1, rows: CountRows(worksheet), shiftOtherRowsUp: true);
+			WorksheetCannotBeNull(worksheet);
+
+			worksheet.DeleteRow(rowFrom: MIN_ROW_INDEX, rows: CountRows(worksheet), shiftOtherRowsUp: true);
+		}
+
+		private static void ExcelPackageCannotBeNull(ExcelPackage excelPackage)
+		{
+			if(excelPackage == null)
+				throw new ArgumentException("ExcelPackage cannot be null.");
+		}
+
+		private static void WorksheetCannotBeNull(ExcelWorksheet worksheet)
+		{
+			if(worksheet == null)
+				throw new ArgumentException("Worksheet cannot be null.");
 		}
 	}
 }
---
+++ DataFilesTest/ExcelPackageHelperTests.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using WithoutHaste.DataFiles;
+
+namespace DataFilesTest
+{
+	[TestClass]
+	public class ExcelPackageHelperTests
+	{
+		[TestMethod]
+		public void ExcelPackageHelper_AddWorksheet()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			//act
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			//assert
+			Assert.AreEqual(name, worksheet.Name);
+			Assert.AreEqual(1, ExcelPackageHelper.CountWorksheets(package));
+			Assert.AreEqual(0, ExcelPackageHelper.CountRows(worksheet));
+			Assert.AreEqual(0, ExcelPackageHelper.CountColumns(worksheet));
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_GetWorksheet()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheetA = ExcelPackageHelper.AddWorksheet(package, name);
+			//act
+			ExcelWorksheet worksheetB = ExcelPackageHelper.GetWorksheet(package, name);
+			//assert
+			Assert.AreEqual(worksheetA, worksheetB);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_GetWorksheet_Null()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			//act
+			ExcelWorksheet worksheet = ExcelPackageHelper.GetWorksheet(package, name);
+			//assert
+			Assert.AreEqual(null, worksheet);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_AppendRow_GetRow()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> row1 = new List<object>() { null, 123, "abc", "", new DateTime(1999, 3, 2) };
+			List<object> row2 = new List<object>() { 456, "", null, null, null, null, 789 };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, row1);
+			ExcelPackageHelper.AppendRow(worksheet, row2);
+			List<object> result1 = ExcelPackageHelper.GetRow(worksheet, 1);
+			List<object> result2 = ExcelPackageHelper.GetRow(worksheet, 2);
+			//assert
+			Assert.AreEqual(2, ExcelPackageHelper.CountRows(worksheet));
+			Assert.AreEqual(Math.Max(row1.Count, row2.Count), ExcelPackageHelper.CountColumns(worksheet));
+			for(int i = 0; i < row1.Count; i++)
+			{
+				Assert.AreEqual(row1[i], result1[i]);
+			}
+			for(int i = 0; i < row2.Count; i++)
+			{
+				Assert.AreEqual(row2[i], result2[i]);
+			}
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_SetColumnByIndex_GetColumnByIndex()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> col1 = new List<object>() { null, 123, "abc", "", new DateTime(1999, 3, 2) };
+			List<object> col2 = new List<object>() { 456, "", null, null, null, null, 789 };
+			//act
+			ExcelPackageHelper.SetColumnByIndex(worksheet, 1, col1, skipFirstRow:false);
+			ExcelPackageHelper.SetColumnByIndex(worksheet, 2, col2, skipFirstRow:false);
+			List<object> result1 = ExcelPackageHelper.GetColumnByIndex(worksheet, 1, skipFirstRow:false);
+			List<object> result2 = ExcelPackageHelper.GetColumnByIndex(worksheet, 2, skipFirstRow:false);
+			//assert
+			Assert.AreEqual(2, ExcelPackageHelper.CountColumns(worksheet));
+			Assert.AreEqual(Math.Max(col1.Count, col2.Count), ExcelPackageHelper.CountRows(worksheet));
+			for(int i = 0; i < col1.Count; i++)
+			{
+				Assert.AreEqual(col1[i], result1[i]);
+			}
+			for(int i = 0; i < col2.Count; i++)
+			{
+				Assert.AreEqual(col2[i], result2[i]);
+			}
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_SetColumnByChar_GetColumnByChar()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> col1 = new List<object>() { null, 123, "abc", "", new DateTime(1999, 3, 2) };
+			List<object> col2 = new List<object>() { 456, "", null, null, null, null, 789 };
+			//act
+			ExcelPackageHelper.SetColumnByChar(worksheet, "A", col1, skipFirstRow: false);
+			ExcelPackageHelper.SetColumnByChar(worksheet, "B", col2, skipFirstRow: false);
+			List<object> result1 = ExcelPackageHelper.GetColumnByChar(worksheet, "A", skipFirstRow: false);
+			List<object> result2 = ExcelPackageHelper.GetColumnByChar(worksheet, "B", skipFirstRow: false);
+			//assert
+			Assert.AreEqual(2, ExcelPackageHelper.CountColumns(worksheet));
+			Assert.AreEqual(Math.Max(col1.Count, col2.Count), ExcelPackageHelper.CountRows(worksheet));
+			for(int i = 0; i < col1.Count; i++)
+			{
+				Assert.AreEqual(col1[i], result1[i]);
+			}
+			for(int i = 0; i < col2.Count; i++)
+			{
+				Assert.AreEqual(col2[i], result2[i]);
+			}
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_SetColumnByHeader_GetColumnByHeader()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<string> headers = new List<string>() { "ColA", "ColB" };
+			List<object> col1 = new List<object>() { null, 123, "abc", "", new DateTime(1999, 3, 2) };
+			List<object> col2 = new List<object>() { 456, "", null, null, null, null, 789 };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, headers);
+			ExcelPackageHelper.SetColumnByHeader(worksheet, headers[0], col1);
+			ExcelPackageHelper.SetColumnByHeader(worksheet, headers[1], col2);
+			List<object> result1 = ExcelPackageHelper.GetColumnByHeader(worksheet, headers[0]);
+			List<object> result2 = ExcelPackageHelper.GetColumnByHeader(worksheet, headers[1]);
+			//assert
+			Assert.AreEqual(headers[0], worksheet.Cells["A1"].Value.ToString());
+			Assert.AreEqual(headers[1], worksheet.Cells["B1"].Value.ToString());
+			Assert.AreEqual(2, ExcelPackageHelper.CountColumns(worksheet));
+			Assert.AreEqual(Math.Max(col1.Count, col2.Count) + 1, ExcelPackageHelper.CountRows(worksheet));
+			for(int i = 0; i < col1.Count; i++)
+			{
+				Assert.AreEqual(col1[i], result1[i]);
+			}
+			for(int i = 0; i < col2.Count; i++)
+			{
+				Assert.AreEqual(col2[i], result2[i]);
+			}
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_GetColumnCharForHeader()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<string> headers = new List<string>() { "ColA", "ColB", "ColC" };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, headers);
+			string a = ExcelPackageHelper.GetColumnCharForHeader(worksheet, headers[0]);
+			string b = ExcelPackageHelper.GetColumnCharForHeader(worksheet, headers[1]);
+			string c = ExcelPackageHelper.GetColumnCharForHeader(worksheet, headers[2]);
+			string d = ExcelPackageHelper.GetColumnCharForHeader(worksheet, "Other");
+			//assert
+			Assert.AreEqual("A", a);
+			Assert.AreEqual("B", b);
+			Assert.AreEqual("C", c);
+			Assert.IsNull(d);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_CountRows_FromRows()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<string> row = new List<string>() { "ColA", "ColB", "ColC" };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			int count = ExcelPackageHelper.CountRows(worksheet);
+			//assert
+			Assert.AreEqual(4, count);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_CountRows_FromColumns()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> colA = new List<object>() { "ColA", "ColB", "ColC" };
+			List<object> colB = new List<object>() { "ColA" };
+			List<object> colC = new List<object>() { "ColA", "ColB", "ColC", "ColD", "ColE" };
+			//act
+			ExcelPackageHelper.SetColumnByIndex(worksheet, 1, colA, skipFirstRow:false);
+			ExcelPackageHelper.SetColumnByIndex(worksheet, 2, colB, skipFirstRow:false);
+			ExcelPackageHelper.SetColumnByIndex(worksheet, 3, colC, skipFirstRow:false);
+			int count = ExcelPackageHelper.CountRows(worksheet);
+			//assert
+			Assert.AreEqual(5, count);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_CountColumns_FromRows()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> rowA = new List<object>() { "ColA", "ColB", "ColC" };
+			List<object> rowB = new List<object>() { "ColA" };
+			List<object> rowC = new List<object>() { "ColA", "ColB", "ColC", "ColD", "ColE" };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, rowA);
+			ExcelPackageHelper.AppendRow(worksheet, rowB);
+			ExcelPackageHelper.AppendRow(worksheet, rowC);
+			int count = ExcelPackageHelper.CountColumns(worksheet);
+			//assert
+			Assert.AreEqual(5, count);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_CountColumns_FromColumns()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<object> colB = new List<object>() { "ColA", "ColB", "ColC" };
+			List<object> colD = new List<object>() { "ColA" };
+			List<object> colF = new List<object>() { "ColA", "ColB", "ColC", "ColD", "ColE" };
+			//act
+			ExcelPackageHelper.SetColumnByChar(worksheet, "B", colB, skipFirstRow:false);
+			ExcelPackageHelper.SetColumnByChar(worksheet, "D", colD, skipFirstRow:false);
+			ExcelPackageHelper.SetColumnByChar(worksheet, "F", colF, skipFirstRow:false);
+			int count = ExcelPackageHelper.CountColumns(worksheet);
+			//assert
+			Assert.AreEqual(6, count);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_CountWorksheets()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			//act
+			ExcelPackageHelper.AddWorksheet(package, name+1);
+			ExcelPackageHelper.AddWorksheet(package, name+2);
+			ExcelPackageHelper.AddWorksheet(package, name+3);
+			int count = ExcelPackageHelper.CountWorksheets(package);
+			//assert
+			Assert.AreEqual(3, count);
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_ColumnChar()
+		{
+			//arrange
+			Dictionary<int, string> expectedValues = new Dictionary<int, string>() {
+				{ 1, "A" },
+				{ 2, "B" },
+				{ 25, "Y" },
+				{ 26, "Z" },
+				{ 27, "AA" },
+				{ 28, "AB" }
+			};
+			//act
+			//assert
+			foreach(KeyValuePair<int, string> expectedValue in expectedValues)
+			{
+				Assert.AreEqual(expectedValue.Value, ExcelPackageHelper.ColumnChar(expectedValue.Key));
+			}
+		}
+
+		[TestMethod]
+		public void ExcelPackageHelper_Clear()
+		{
+			//arrange
+			ExcelPackage package = new ExcelPackage();
+			string name = "Title";
+			ExcelWorksheet worksheet = ExcelPackageHelper.AddWorksheet(package, name);
+			List<string> row = new List<string>() { "ColA", "ColB", "ColC" };
+			//act
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.AppendRow(worksheet, row);
+			ExcelPackageHelper.Clear(worksheet);
+			int rowCount = ExcelPackageHelper.CountRows(worksheet);
+			int columnCount = ExcelPackageHelper.CountColumns(worksheet);
+			//assert
+			Assert.AreEqual(0, rowCount);
+			Assert.AreEqual(0, columnCount);
+		}
+	}
+}
