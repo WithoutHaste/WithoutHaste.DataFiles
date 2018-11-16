f6f5e223b0e35ed65943426128f7e7dccb7b2f64
J M <wohaste@gmail.com>
2018-11-16 09:22:31 -0700

    DotNet: organize namespaces into tree

---
+++ DataFiles/DotNet/DotNetQualifiedClassNameTreeNode.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithoutHaste.DataFiles.DotNet
+{
+	/// <summary>
+	/// A node in a tree data structure made up of DotNetQualifiedClassNames organized by their namespaces.
+	/// </summary>
+	public class DotNetQualifiedClassNameTreeNode
+	{
+		public DotNetQualifiedClassNameTreeNode Parent { get; protected set; }
+		public DotNetQualifiedClassName Value { get; protected set; }
+		public List<DotNetQualifiedClassNameTreeNode> Children { get; protected set; }
+
+		internal DotNetQualifiedClassNameTreeNode(DotNetQualifiedClassName value)
+		{
+			Value = value;
+			Parent = null;
+			Children = new List<DotNetQualifiedClassNameTreeNode>();
+		}
+
+		/// <summary>
+		/// Organize a list of namespaces into a tree, based on which namespaces are within other namespaces.
+		/// </summary>
+		/// <returns>
+		/// Returns the root of the new tree.
+		/// If there is one top-level namespace, root.Value will be that namespace.
+		/// If there are more than one top-level namespaces, root.Value will be null and root.Children will contain the top-level namespaces.
+		/// </returns>
+		public static DotNetQualifiedClassNameTreeNode Generate(List<DotNetQualifiedClassName> names)
+		{
+			if(names == null || names.Count == 0)
+				return new DotNetQualifiedClassNameTreeNode(null);
+
+			DotNetQualifiedClassNameTreeNode root = new DotNetQualifiedClassNameTreeNode(null);
+			foreach(DotNetQualifiedClassName name in names)
+			{
+				root.Insert(name);
+			}
+			return root;
+		}
+
+		/// <summary>
+		/// Insert a new namespace into its proper position, based on the current node as the root.
+		/// </summary>
+		public void Insert(DotNetQualifiedClassName name)
+		{
+			//first root
+			if(this.Value == null && this.Children.Count == 0)
+			{
+				this.Value = name;
+				return;
+			}
+
+			if(this.Value == null) //null root over multiple top-level namespaces
+			{
+				//this belongs below a child
+				foreach(DotNetQualifiedClassNameTreeNode childNode in this.Children)
+				{
+					if(name.IsWithin(childNode.Value))
+					{
+						childNode.Insert(name);
+						return;
+					}
+				}
+				//this is at the same level as children
+				DotNetQualifiedClassNameTreeNode newNode = new DotNetQualifiedClassNameTreeNode(name);
+				this.AddChild(newNode);
+				//children that belong below the new node
+				int index = 0;
+				while(index < this.Children.Count)
+				{
+					if(this.Children[index].Value.IsWithin(name))
+					{
+						this.TransferChildTo(this.Children[index], newNode);
+						continue;
+					}
+					index++;
+				}
+				//all children are within new name
+				if(this.Children.Count == 1)
+				{
+					newNode.TransferChildrenTo(this);
+					this.Value = newNode.Value;
+					this.Children.Remove(newNode);
+				}
+			}
+			else //normal, full tree
+			{
+				if(this.Value.IsWithin(name)) //new root
+				{
+					DotNetQualifiedClassNameTreeNode rootTransfer = new DotNetQualifiedClassNameTreeNode(this.Value);
+					this.TransferChildrenTo(rootTransfer);
+					this.Value = name;
+					this.AddChild(rootTransfer);
+					return;
+				}
+				else if(name.IsWithin(this.Value)) //descendent of root
+				{
+					//belongs below a child
+					foreach(DotNetQualifiedClassNameTreeNode childNode in this.Children)
+					{
+						if(name.IsWithin(childNode.Value))
+						{
+							childNode.Insert(name);
+							return;
+						}
+					}
+					//belongs at same level as children
+					DotNetQualifiedClassNameTreeNode newNode = new DotNetQualifiedClassNameTreeNode(name);
+					this.AddChild(newNode);
+					//children that belong below the new node
+					int index = 0;
+					while(index < this.Children.Count)
+					{
+						if(this.Children[index].Value.IsWithin(name))
+						{
+							this.TransferChildTo(this.Children[index], newNode);
+							continue;
+						}
+						index++;
+					}
+				}
+				else //same level as root
+				{
+					DotNetQualifiedClassNameTreeNode rootTransfer = new DotNetQualifiedClassNameTreeNode(this.Value);
+					this.TransferChildrenTo(rootTransfer);
+					this.Value = null;
+					this.AddChild(rootTransfer);
+					DotNetQualifiedClassNameTreeNode newNode = new DotNetQualifiedClassNameTreeNode(name);
+					this.AddChild(newNode);
+				}
+			}
+		}
+
+		private void TransferChildrenTo(DotNetQualifiedClassNameTreeNode other)
+		{
+			while(this.Children.Count > 0)
+			{
+				other.AddChild(this.Children[0]);
+				this.Children.RemoveAt(0);
+			}
+		}
+
+		private void TransferChildTo(DotNetQualifiedClassNameTreeNode child, DotNetQualifiedClassNameTreeNode other)
+		{
+			other.AddChild(child);
+			this.Children.Remove(child);
+		}
+
+		private void AddChild(DotNetQualifiedClassNameTreeNode child)
+		{
+			Children.Add(child);
+			child.Parent = this;
+		}
+	}
+}
---
+++ DataFilesTest/DotNet/DotNetQualifiedClassNameTreeNodeTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WithoutHaste.DataFiles.DotNet;
+
+namespace DataFilesTest
+{
+	[TestClass]
+	public class DotNetQualifiedClassNameTreeNodeTests
+	{
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_Null()
+		{
+			//arrange
+			List<DotNetQualifiedClassName> names = null;
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(names);
+			//assert
+			Assert.IsNull(root.Value);
+			Assert.AreEqual(0, root.Children.Count);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_Empty()
+		{
+			//arrange
+			List<DotNetQualifiedClassName> names = new List<DotNetQualifiedClassName>();
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(names);
+			//assert
+			Assert.IsNull(root.Value);
+			Assert.AreEqual(0, root.Children.Count);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_One()
+		{
+			//arrange
+			List<DotNetQualifiedClassName> names = new List<DotNetQualifiedClassName>() {
+				DotNetQualifiedClassName.FromVisualStudioXml("NameA.ClassA")
+			};
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(names);
+			//assert
+			Assert.AreEqual("NameA.ClassA", root.Value);
+			Assert.AreEqual(0, root.Children.Count);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_FirstRoot()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.ClassA");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			//assert
+			Assert.AreEqual(a, root.Value);
+			Assert.AreEqual(0, root.Children.Count);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_PushDownRoot()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.ClassA");
+			DotNetQualifiedClassName b = DotNetQualifiedClassName.FromVisualStudioXml("NameA");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(b);
+			//assert
+			Assert.AreEqual(b, root.Value);
+			Assert.AreEqual(1, root.Children.Count);
+			Assert.AreEqual(a, root.Children[0].Value);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_ChildOfRoot()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName b = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameC");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(b);
+			//assert
+			Assert.AreEqual(a, root.Value);
+			Assert.AreEqual(1, root.Children.Count);
+			Assert.AreEqual(b, root.Children[0].Value);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_DescendentOfRoot()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName c = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameC");
+			DotNetQualifiedClassName d = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameD");
+			DotNetQualifiedClassName e = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameD.NameE");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(c);
+			root.Insert(d);
+			root.Insert(e);
+			//assert
+			Assert.AreEqual(a, root.Value);
+			Assert.AreEqual(2, root.Children.Count);
+			Assert.AreEqual(c, root.Children[0].Value);
+			Assert.AreEqual(d, root.Children[1].Value);
+			Assert.AreEqual(1, root.Children[1].Children.Count);
+			Assert.AreEqual(e, root.Children[1].Children[0].Value);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_BetweenRootAndChild()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName c = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameC");
+			DotNetQualifiedClassName d = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler.NameD");
+			DotNetQualifiedClassName e = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB.Filler");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(c);
+			root.Insert(d);
+			root.Insert(e);
+			//assert
+			Assert.AreEqual(a, root.Value);
+			Assert.AreEqual(1, root.Children.Count);
+			Assert.AreEqual(e, root.Children[0].Value);
+			Assert.AreEqual(2, root.Children[0].Children.Count);
+			Assert.AreEqual(c, root.Children[0].Children[0].Value);
+			Assert.AreEqual(d, root.Children[0].Children[1].Value);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_MultipleRoots()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName c = DotNetQualifiedClassName.FromVisualStudioXml("NameC");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(c);
+			//assert
+			Assert.AreEqual(null, root.Value);
+			Assert.AreEqual(2, root.Children.Count);
+			Assert.AreEqual(a, root.Children[0].Value);
+			Assert.AreEqual(0, root.Children[0].Children.Count);
+			Assert.AreEqual(c, root.Children[1].Value);
+			Assert.AreEqual(0, root.Children[1].Children.Count);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_MultipleRootsChild()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName c = DotNetQualifiedClassName.FromVisualStudioXml("NameC");
+			DotNetQualifiedClassName d = DotNetQualifiedClassName.FromVisualStudioXml("NameC.NameD");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(c);
+			root.Insert(d);
+			//assert
+			Assert.AreEqual(null, root.Value);
+			Assert.AreEqual(2, root.Children.Count);
+			Assert.AreEqual(a, root.Children[0].Value);
+			Assert.AreEqual(0, root.Children[0].Children.Count);
+			Assert.AreEqual(c, root.Children[1].Value);
+			Assert.AreEqual(1, root.Children[1].Children.Count);
+			Assert.AreEqual(d, root.Children[1].Children[0].Value);
+		}
+
+		[TestMethod]
+		public void DotNetQualifiedClassNameTreeNode_Generate_MultipleRootsChildBetween()
+		{
+			//arrange
+			DotNetQualifiedClassName a = DotNetQualifiedClassName.FromVisualStudioXml("NameA.NameB");
+			DotNetQualifiedClassName d = DotNetQualifiedClassName.FromVisualStudioXml("NameC.NameD");
+			DotNetQualifiedClassName c = DotNetQualifiedClassName.FromVisualStudioXml("NameC");
+			//act
+			DotNetQualifiedClassNameTreeNode root = DotNetQualifiedClassNameTreeNode.Generate(null);
+			root.Insert(a);
+			root.Insert(d);
+			root.Insert(c);
+			//assert
+			Assert.AreEqual(null, root.Value);
+			Assert.AreEqual(2, root.Children.Count);
+			Assert.AreEqual(a, root.Children[0].Value);
+			Assert.AreEqual(0, root.Children[0].Children.Count);
+			Assert.AreEqual(c, root.Children[1].Value);
+			Assert.AreEqual(1, root.Children[1].Children.Count);
+			Assert.AreEqual(d, root.Children[1].Children[0].Value);
+		}
+	}
+}
