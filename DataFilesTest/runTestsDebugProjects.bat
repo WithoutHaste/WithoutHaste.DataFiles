dcc992c5ce0307168cf3adcf4d73443cd46b2ed5
J M <wohaste@gmail.com>
2019-03-15 14:52:59 -0600

    document batch file

--- DataFilesTest/runTestsDebugProjects.bat
+++ DataFilesTest/runTestsDebugProjects.bat
@@ -1,7 +1,9 @@
+REM remove everything from the logs folder so new results can be saved
 set folder="logs"
 cd /d %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 cd ..
+REM run tests
 mstest /testcontainer:bin\Debug\DataFilesTest.net20.dll /resultsfile:logs\DataFilesTestOutput.net20.Debug.trx
 mstest /testcontainer:bin\Debug\DataFilesTest.net30.dll /resultsfile:logs\DataFilesTestOutput.net30.Debug.trx
 mstest /testcontainer:bin\Debug\DataFilesTest.net35.dll /resultsfile:logs\DataFilesTestOutput.net35.Debug.trx
